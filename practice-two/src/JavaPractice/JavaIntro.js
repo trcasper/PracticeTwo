ef77090dddb3671bd8ed1ce7860a65a403dc2d29
trcasper <trcasper90@gmail.com>
2020-05-25 22:00:51 -0600

    first commit

---
+++ practice-two/src/JavaPractice/JavaIntro.js
@@ -0,0 +1,35 @@
+INTRODUCTION TO JAVASCRIPT
+What is JavaScript?
+Last year, millions of learners from our community started with JavaScript. Why? JavaScript is primarily known as the language of most modern web browsers, and its early quirks gave it a bit of a bad reputation. However, the language has continued to evolve and improve. JavaScript is a powerful, flexible, and fast programming language now being used for increasingly complex web development and beyond!
+
+Since JavaScript remains at the core of web development, it’s often the first language learned by self-taught coders eager to learn and build. We’re excited for what you’ll be able to create with the JavaScript foundation you gain here. JavaScript powers the dynamic behavior on most websites, including this one.
+
+In this lesson, you will learn introductory coding concepts including data types and built-in objects—essential knowledge for all aspiring developers. Make sure to take notes and pace yourself. This foundation will set you up for understanding the more complex concepts you’ll encounter later.
+
+Console
+The console is a panel that displays important messages, like errors, for developers. Much of the work the computer does with our code is invisible to us by default. If we want to see things appear on our screen, we can print, or log, to our console directly.
+
+In JavaScript, the console keyword refers to an object, a collection of data and actions, that we can use in our code. Keywords are words that are built into the JavaScript language, so the computer will recognize them and treats them specially.
+
+One action, or method, that is built into the console object is the .log() method. When we write console.log() what we put inside the parentheses will get printed, or logged, to the console.
+
+It’s going to be very useful for us to print values to the console, so we can see the work that we’re doing.
+
+console.log(5); 
+This example logs 5 to the console. The semicolon denotes the end of the line, or statement. Although in JavaScript your code will usually run as intended without a semicolon, we recommend learning the habit of ending each statement with a semicolon so you never leave one out in the few instances when they are required.
+
+You’ll see later on that we can use console.log() to print different kinds of data.
+
+1.
+Use the console.log code in the editor to log your age to the console.
+
+Run your code when you are ready to see the result.
+
+
+Stuck? Get a hint
+2.
+On the next line, log a number representing the number of weeks you’ve been programming.
+
+console.log(30);
+
+console.log(50);
